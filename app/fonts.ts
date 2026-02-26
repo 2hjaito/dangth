5d4a4322e71fbba6d484c9184448676b29b18254
2hjaito <dangtt135@gmail.com>
2026-02-26 11:00:38 +0700

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,49 +1,5 @@
 import localFont from 'next/font/local'
 
-export const cmuSans = localFont({
-  src: [
-    {
-      path: '../public/fonts/cmu/vi/CMUSansSerifVietnamized.woff',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/cmu/sans/cmunss.woff',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/cmu/sans/cmunsx.woff',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../public/fonts/cmu/sans/cmunsi.woff',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: '../public/fonts/cmu/sans/cmunso.woff',
-      weight: '700',
-      style: 'italic',
-    },
-  ],
-  variable: '--font-cmusans',
-  display: 'swap',
-})
-
-export const iBMPlexSans = localFont({
-  src: [
-    {
-      path: '../public/fonts/IBM-PlexSans/IBMPlexSans-Regular.woff',
-      weight: '400',
-      style: 'normal',
-    },
-  ],
-  variable: '--font-iBMPlexSans',
-  display: 'swap',
-})
-
 export const cmuSansVi = localFont({
   src: [
     {
@@ -52,27 +8,13 @@ export const cmuSansVi = localFont({
       style: 'normal',
     },
     {
-      path: '../public/fonts/cmu/sans/cmunss.woff',
-      weight: '500',
-      style: 'normal',
-    },
-    {
       path: '../public/fonts/cmu/sans/cmunsx.woff',
       weight: '700',
       style: 'normal',
     },
-    {
-      path: '../public/fonts/cmu/sans/cmunsi.woff',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: '../public/fonts/cmu/sans/cmunso.woff',
-      weight: '700',
-      style: 'italic',
-    },
   ],
   variable: '--font-cmusans-vi',
   display: 'swap',
+  preload: false,
 })
 
