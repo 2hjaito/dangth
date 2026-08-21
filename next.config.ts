825fbc210951b27d26d0b7972cbbe0730140cebc
Tran Huu Dang <89396036+2hjaito@users.noreply.github.com>
2026-08-21 16:27:32 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     qualities: [60, 70, 75],
   },
+  outputFileTracingExcludes: {
+    '/*': ['./public/**/*'],
+  },
 };
 
 export default nextConfig;
